ed62bcdd7a6c77fa3f5ea873ae4ea8a1454d7b1d
Martin Sione <martinsione03@gmail.com>
2022-01-25 13:01:58 -0300

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import App from "@/components/App";
+import "normalize.css";
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
